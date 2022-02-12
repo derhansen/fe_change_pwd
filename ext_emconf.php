004e177b1f5931bd694afa807c933cfdc5f72481
Torben Hansen <derhansen@gmail.com>
2022-02-12 12:23:36 +0100

    [TASK] Fixed TYPO3 version compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.3.0-11.5.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
