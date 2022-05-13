26007284106d640f965f2ed322fd7d47080486cc
Christian Weiske <weiske@mogic.com>
2022-05-13 14:24:39 +0200

    [TASK] Fix deprecation notice on TYPO3v10: Use class name when configuring plugin
    Deprecation message:
    > TYPO3 Deprecation Notice:
    > Calling TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin for
    > extension ("FeChangePwd") and plugin ("Pi1") with controller aliases in
    > argument $controllerActions is deprecated and will stop working in TYPO3 11.0.
    
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/10.0/Deprecation-87550-UseControllerClassesWhenRegisteringPluginsmodules.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,15 +2,16 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
+    $passwordControllerClass = version_compare(TYPO3_version, '10.0', '>=') ? \Derhansen\FeChangePwd\Controller\PasswordController::class : 'Password';
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'Derhansen.fe_change_pwd',
         'Pi1',
         [
-            'Password' => 'edit,update',
+            $passwordControllerClass => 'edit,update',
         ],
         // non-cacheable actions
         [
-            'Password' => 'edit,update',
+            $passwordControllerClass => 'edit,update',
         ]
     );
 });
