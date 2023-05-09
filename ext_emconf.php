d531ab9f46a8569224005545bd5a8e3991ec3978
Torben Hansen <derhansen@gmail.com>
2023-05-09 14:22:52 +0200

    [RELEASE] Release of version to 4.0.0
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Change password for frontend users',
-    'description' => 'Plugin to enable password change for frontend users. Contains configurable password 
+    'description' => 'Plugin to enable password change for frontend users. Contains configurable password
         rules and password change enforcement.',
     'category' => 'plugin',
     'author' => 'Torben Hansen',
     'author_email' => 'torben@derhansen.com',
     'state' => 'stable',
-    'version' => '4.0.0-dev',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99'
