a52e104e645904b475af0f3bda0f75cb4e4503e9
Torben Hansen <derhansen@gmail.com>
2024-10-20 18:34:38 +0200

    [TASK] Use core abstract class for list_type to CType migration
    Closes #91

---
+++ Classes/Updates/PluginToContentElementUpdater.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Derhansen\FeChangePwd\Updates;
+
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+#[UpgradeWizard('feChangePwdPluginToContentElementUpdate')]
+class PluginToContentElementUpdater extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'fechangepwd_pi1' => 'fechangepwd_pi1',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'ext:fe_change_pwd: Migrate plugins to content elements';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Migrates existing plugin records and backend user permissions used by ext:fe_change_pwd.';
+    }
+}
